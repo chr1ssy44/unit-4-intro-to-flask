00a395fe956011b2eabb1a4e8aabbfe758dc4555
chr1ssy44 <cbeckford2@brooklynsteamcenter.org>
2024-01-02 10:50:50 -0500

    added a file

--- todo.py
+++ todo.py
@@ -1,12 +1,20 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
 todos = ['get my nails done','spend time with my family']
 
-@app.route('/', methods =['GET','POST'])
+@app.route('/', methods = ['GET','POST'])
 def index():
-    new_todo = request.form["new_todo"]
-    todos.append(new_todo)
+    if request.method == 'POST':
+        new_todo = request.form["new_todo"]
+        todos.append(new_todo)
+
     return render_template('todo.html.jinja', my_todos=todos)
 
+
+@app.route('/delete_todo/<int:todo_index>', methods = ['POST'])
+def todo_delete(todo_index):
+    del todos[todo_index]
+
+    return redirect('/')
