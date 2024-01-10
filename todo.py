f4d9d3107b0663f524d54fd1904a1d1ee3dcd0c1
chr1ssy44 <cbeckford2@brooklynsteamcenter.org>
2024-01-10 10:52:01 -0500

    added a file

--- todo.py
+++ todo.py
@@ -1,27 +1,8 @@
 from flask import Flask, render_template, request, redirect
 import pymysql
 import pymysql.cursors
-from pprint import pprint as print
 
 app = Flask(__name__)
-
-results = ['get my nails done','spend time with my family']
-
-
-@app.route('/', methods = ['GET','POST'])
-def index():
-    if request.method == 'POST':
-        new_todo = request.form["new_todo"]
-        results.append(new_todo)
-
-    return render_template('todo.html.jinja', my_todos=results)
-
-
-@app.route('/delete_todo/<int:todo_index>', methods = ['POST'])
-def todo_delete(todo_index):
-    del results[todo_index]
-
-    return redirect('/')
 
 conn = pymysql.connect(
     database = "cbeckford2_todos",
@@ -30,9 +11,42 @@ conn = pymysql.connect(
     host = "10.100.33.60",
     cursorclass = pymysql.cursors.DictCursor
 )
-cursor = conn.cursor()
 
-cursor.execute("SELECT `description` FROM `todos`")
+@app.route('/', methods = ['GET','POST'])
+def index():
+    if request.method == 'POST':
+        new_todo = request.form["new_todo"]
+        todos.append(new_todo)
+        cursor = conn.cursor()
+        cursor.execute(f"INSERT INTO `todos` (`description`) VALUES ('{new_todo}')")
+        cursor.close()
+        conn.commit()
 
-results = cursor.fetchall()
-print(results)
+    cursor = conn.cursor()
+
+    cursor.execute("SELECT * from `todos` ORDER BY `complete`")
+    
+    results = cursor.fetchall()
+    
+    cursor.close()
+    return render_template('todo.html.jinja', my_todos=results)
+
+todos = ['get my nails done','spend time with my family']
+
+
+@app.route('/delete_todo/<int:todo_index>', methods = ['POST'])
+def todo_delete(todo_index):
+    cursor = conn.cursor()
+    cursor.execute(f"DELETE FROM `todos` WHERE `id` = {todo_index}")
+    cursor.close()
+    conn.commit()
+    return redirect('/')
+    
+@app.route('/complete_todo/<int:todo_index>', methods = ['POST'])
+def todo_complete(todo_index):
+    cursor = conn.cursor()
+    cursor.execute(f"UPDATE `todos` SET `complete` = 1 WHERE `id` = {todo_index}")
+    cursor.close()
+    conn.commit()
+    return redirect('/')
+    
