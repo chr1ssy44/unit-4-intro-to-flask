0cd298849a899c2d8b6944df965fa4594070274e
chr1ssy44 <cbeckford2@brooklynsteamcenter.org>
2024-01-11 10:46:39 -0500

    added a file

--- todo.py
+++ todo.py
@@ -1,8 +1,16 @@
 from flask import Flask, render_template, request, redirect
 import pymysql
 import pymysql.cursors
+from flask_httpauth import HTTPBasicAuth
+from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
+auth = HTTPBasicAuth()
+
+users = {
+    "christina": generate_password_hash("hello"),
+    "john": generate_password_hash("bye")
+}
 
 conn = pymysql.connect(
     database = "cbeckford2_todos",
@@ -12,7 +20,14 @@ conn = pymysql.connect(
     cursorclass = pymysql.cursors.DictCursor
 )
 
+@auth.verify_password
+def verify_password(username,password):
+    if username in users and \
+    check_password_hash(users.get(username),password):
+        return username
+
 @app.route('/', methods = ['GET','POST'])
+@auth.login_required
 def index():
     if request.method == 'POST':
         new_todo = request.form["new_todo"]
@@ -30,6 +45,8 @@ def index():
     
     cursor.close()
     return render_template('todo.html.jinja', my_todos=results)
+if __name__ == '__main__':
+    app.run()
 
 todos = ['get my nails done','spend time with my family']
 
@@ -49,4 +66,4 @@ def todo_complete(todo_index):
     cursor.close()
     conn.commit()
     return redirect('/')
-    
+
